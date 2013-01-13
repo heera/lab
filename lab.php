6005058479fe0dee4a6336da40a4af3c93a0dc2b
Matthew J. Sahagian <msahagian@dotink.org>
2013-01-13 02:47:04 -0800

    Removed Colors dependency

--- lab.php
+++ lab.php
@@ -2,7 +2,6 @@
 
 	include(__DIR__ . '/library/parody/Load.php');
 	include(__DIR__ . '/library/Assert.php');
-	include(__DIR__ . '/library/Colors.php');
 
 	use Dotink\Parody;
 
@@ -12,7 +11,6 @@
 
 	const REGEX_ABSOLUTE_PATH = '#^(/|\\\\|[a-z]:(\\\\|/)|\\\\|//)#i';
 
-
 	/**
 	 * Get a message printed in a particular color
 	 *
@@ -21,11 +19,26 @@
 	 * @return string The colored message for CLI output
 	 */
 	function _($message, $color) {
-		static $ansi = NULL;
+		$colors = [
+			'black'        => '0;30',
+			'dark_gray'    => '1;30',
+			'blue'         => '0;34',
+			'light_blue'   => '1;34',
+			'green'        => '0;32',
+			'light_green'  => '1;32',
+			'cyan'         => '0;36',
+			'light_cyan'   => '1;36',
+			'red'          => '0;31',
+			'light_red'    => '1;31',
+			'purple'       => '0;35',
+			'light_purple' => '1;35',
+			'brown'        => '0;33',
+			'yellow'       => '1;33',
+			'light_gray'   => '0;37',
+			'white'        => '1;37'
+		];
 
-		$ansi = new \Colors();
-
-		return $ansi->getColoredString($message, $color);
+		return sprintf("\033[%sm%s\033[0m", $colors[$color], $message);
 	}
 
 
